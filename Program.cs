513d772b49b7ed96fee598d348caaacf6f46f351
oguzkose <oguzkose2019@outlook.com>
2021-08-05 00:24:28 +0300

    Program.cs changed

--- Program.cs
+++ Program.cs
@@ -6,21 +6,7 @@ namespace stack_and_heap
     {
         static void Main(string[] args)
         {
-            int [] numbers = new int[] {1,2,3,4,5};
-
-            numbers[4] = 6;
-
-            foreach (var item in numbers)
-            {
-                System.Console.WriteLine(item);
-            }
-
-        }
-    }
-    
-}
-
-   /*----------STACK-----------*/
+            /*----------STACK-----------*/
         //  int number1 = 10;
         //  int number2 = 20;
 
@@ -30,4 +16,21 @@ namespace stack_and_heap
         //  number2=30;
         //  System.Console.WriteLine(number2); //Output : 30
         //  System.Console.WriteLine(number1); //Output : 20
+
+
             /*----------HEAP-------------*/
+        // int [] numbers = new int[] {1,2,3,4,5};
+
+        // numbers[4] = 6;
+
+        // foreach (var item in numbers)
+        // {
+        //     System.Console.WriteLine(item);
+        // }
+
+        }
+    }
+    
+}
+
+   
